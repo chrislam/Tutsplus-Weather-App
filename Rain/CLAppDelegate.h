716b4e9670c4a60c4e1e898cbf7189856c3b4cc9
Chris Lam <chris2trees@gmail.com>
2013-10-09 08:14:54 +0100

    Initial Commit

---
+++ Rain/CLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLAppDelegate.h
+//  Rain
+//
+//  Created by Chris Lam on 9/10/13.
+//  Copyright (c) 2013 Chris Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Rain/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rain
+//
+//  Created by Chris Lam on 9/10/13.
+//  Copyright (c) 2013 Chris Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLAppDelegate class]));
+    }
+}
